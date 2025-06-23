2e43c0646a8fb3c5a328e9776fbedc288a2669d1
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-06-23 19:56:55 -0300

    feat(security): geração de token JWT com boas práticas e variáveis de ambiente
    - Token JWT configurado com data de expiração (2h) para maior segurança
    - Utilização de variáveis de ambiente para assinatura do token (evita valores hardcoded)
    - Implementado padrão de produção para geração e validação do token
    - Adoção de boas práticas de segurança no uso de JWT

--- src/main/java/med/voll/api/Voll/Med/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/Voll/Med/controller/AutenticacaoController.java
@@ -3,6 +3,9 @@ package med.voll.api.Voll.Med.controller;
 import jakarta.validation.Valid;
 import med.voll.api.Voll.Med.domain.usuario.AutenticacaoService;
 import med.voll.api.Voll.Med.domain.usuario.DadosLogin;
+import med.voll.api.Voll.Med.domain.usuario.Usuario;
+import med.voll.api.Voll.Med.infra.security.DadosTokenJwt;
+import med.voll.api.Voll.Med.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,10 +19,13 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosLogin dadosLogin) {
-        var token = new UsernamePasswordAuthenticationToken(dadosLogin.login(), dadosLogin.senha());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var authenticationTokentoken = new UsernamePasswordAuthenticationToken(dadosLogin.login(), dadosLogin.senha());
+        var authentication = manager.authenticate(authenticationTokentoken);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJwt(tokenJWT));
     }
 }
---
+++ src/main/java/med/voll/api/Voll/Med/infra/security/DadosTokenJwt.java
@@ -0,0 +1,4 @@
+package med.voll.api.Voll.Med.infra.security;
+
+public record DadosTokenJwt(String token) {
+}
